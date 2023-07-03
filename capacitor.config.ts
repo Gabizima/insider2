be2d95e92f815d83c80bed61f49319c2638b652a
Gabriel Bizimana <gabriel.bizimana@outlook.be>
2023-07-03 13:24:16 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'insider',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
